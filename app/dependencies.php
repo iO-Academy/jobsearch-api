8dfaeb0bf1ba87a6d52ee07035dd73740a8fd158
Mike Oram <emailme@mikeoram.co.uk>
2021-12-23 14:42:54 +0000

    Initial commit

---
+++ app/dependencies.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+use DI\ContainerBuilder;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use Monolog\Processor\UidProcessor;
+use Psr\Container\ContainerInterface;
+use Psr\Log\LoggerInterface;
+use Slim\Views\PhpRenderer;
+
+return function (ContainerBuilder $containerBuilder) {
+    $container = [];
+
+    $container[LoggerInterface::class] = function (ContainerInterface $c) {
+        $settings = $c->get('settings');
+
+        $loggerSettings = $settings['logger'];
+        $logger = new Logger($loggerSettings['name']);
+
+        $processor = new UidProcessor();
+        $logger->pushProcessor($processor);
+
+        $handler = new StreamHandler($loggerSettings['path'], $loggerSettings['level']);
+        $logger->pushHandler($handler);
+
+        return $logger;
+    };
+
+    $container['renderer'] = function (ContainerInterface $c) {
+        $settings = $c->get('settings')['renderer'];
+        $renderer = new PhpRenderer($settings['template_path']);
+        return $renderer;
+    };
+
+    $containerBuilder->addDefinitions($container);
+};
