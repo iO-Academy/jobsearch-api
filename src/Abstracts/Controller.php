e2f732e546532532e12b8135e340cde3608597a1
Mike Oram <mike.oram@mayden.co.uk>
2022-01-05 14:33:37 +0000

    fix php docblock

--- src/Abstracts/Controller.php
+++ src/Abstracts/Controller.php
@@ -3,7 +3,6 @@ declare(strict_types=1);
 
 namespace JobSearch\Abstracts;
 
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
 abstract class Controller
@@ -13,7 +12,6 @@ abstract class Controller
      * @param array $data an array of data to be JSON encoded and added to the response
      * @param int $statusCode an optional status code for the response, defaults to 200
      * @return Response a PSR-7 HTTP Response object with JSON data and a custom status code
-     * @throws \Exception Thrown if the data given cannot be encoded
      */
     protected function respondWithJson(Response $response, array $data, int $statusCode = 200): Response
     {
